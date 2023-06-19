df0c8606664593ecae8303a0a2486208c8f308fb
Jesus Marron <jesusmarron7@gmail.com>
2023-06-19 15:46:05 -0500

    lemonade entity implemented

--- src/main/java/com/jesusmarron/lemonadestand/entities/Lemonade.java
+++ src/main/java/com/jesusmarron/lemonadestand/entities/Lemonade.java
@@ -1,5 +1,6 @@
 package com.jesusmarron.lemonadestand.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -9,4 +10,19 @@ public class Lemonade {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
+    private double lemonJuice;
+
+    @Column(nullable = false)
+    private double water;
+
+    @Column(nullable = false)
+    private double sugar;
+
+    @Column(nullable = false)
+    private int iceCubes;
+
+    @Column(nullable = false)
+    private double price;
 }
