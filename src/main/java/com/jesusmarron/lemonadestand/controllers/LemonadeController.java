9d6902ff3cc3833381ca74d8a735f2d422ce4d1a
Jesus Marron <jesusmarron7@gmail.com>
2023-06-21 14:34:23 -0500

    Lemonade Controller and Service implementation

--- src/main/java/com/jesusmarron/lemonadestand/controllers/LemonadeController.java
+++ src/main/java/com/jesusmarron/lemonadestand/controllers/LemonadeController.java
@@ -2,6 +2,7 @@ package com.jesusmarron.lemonadestand.controllers;
 
 import com.jesusmarron.lemonadestand.entities.Lemonade;
 import com.jesusmarron.lemonadestand.services.LemonadeService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("lemonades")
+@AllArgsConstructor
 public class LemonadeController {
 
     private LemonadeService lemonadeService;
+
     @GetMapping
     public List<Lemonade> getAllLemonades() {
         return lemonadeService.getAllLemonades();
--- src/main/java/com/jesusmarron/lemonadestand/services/impl/LemonadeServiceImpl.java
+++ src/main/java/com/jesusmarron/lemonadestand/services/impl/LemonadeServiceImpl.java
@@ -1,16 +1,21 @@
 package com.jesusmarron.lemonadestand.services.impl;
 
 import com.jesusmarron.lemonadestand.entities.Lemonade;
+import com.jesusmarron.lemonadestand.repositories.LemonadeRepository;
 import com.jesusmarron.lemonadestand.services.LemonadeService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class LemonadeServiceImpl implements LemonadeService {
+
+    private LemonadeRepository lemonadeRepository;
 
     @Override
     public List<Lemonade> getAllLemonades() {
-        return null;
+        return lemonadeRepository.findAll();
     }
 }
